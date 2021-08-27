3a7080b58a294f10ca00474d4e7c18cbbabe6016
sh7ema <sh7ema@gmail.com>
2021-08-27 12:04:08 +0300

    test4

---
+++ modules/s3/terraform.tf
@@ -0,0 +1,45 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+
+  #Prevent accidental deletion of this bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  #Run control version for look change history
+  versioning {
+    enabled = true
+  }
+
+  #Run encryption on server
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  tags = {
+    Name = var.bucket_description
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  hash_key = "LockID"
+  name = "my-test-shlema-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "my-test-shlema2"
+    key = "my-test/mykey.tfstate"
+    region = "eu-central-1"
+    dynamodb_table = "my-test-shlema-locks"
+    encrypt = true
+  }
+}
