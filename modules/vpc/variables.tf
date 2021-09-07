da8eb733f6a0bbeb464802185cd9a18362981e33
sh7ema <sh7ema@gmail.com>
2021-09-07 14:50:58 +0300

    add vpc

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,13 @@
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 80
+}
+
+variable "app_name" {
+  type = string
+}
